381036b5e8d5ecccef359afc7cd08e4c6d461abf
tianshaojun <33224748+tianshaojun@users.noreply.github.com>
2021-01-13 09:18:38 +0800

    编程题

---
+++ 44编程题/counter.js
@@ -0,0 +1,5 @@
+let counter = 10;
+const add = () => {
+   console.log(counter);
+};
+export { counter, add };
---
+++ 44编程题/index.js
@@ -0,0 +1,4 @@
+add();
+import {counter,add} from './counter';
+counter += 1;
+console.log(counter);
